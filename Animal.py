e29750fb6ad3ecad5d58fde0dd64b54c3e7be4cf
Tenzin Phuntsok <tenzinphun2004@gmail.com>
2025-02-18 20:33:21 -0500

    Tenzin p

--- Animal.py
+++ Animal.py
@@ -1,7 +1,34 @@
 class Animal:
-    def __init__(self): 
-        print("hello")
+    def __init__(self, name, species, gender, food=0):
+        self.__name = name
+        self.__species = species
+        self.__gender = gender
+        self.__food = food
+        print("hello I am", self.__name)
+
 
     def talk(self):
-        print("hi")
+        print(self.__name)
+
+    def print_species(self):
+        print("I am a",self.__species)
+
+    def print_gender(self):
+        print("I am a",self.__gender)
+
+    def get_food(self):
+        self.__food += 1
+        print("I have this much food:",self.__food)
+
+    def eat_food(self):
+        if self.__food > 0:
+            self.__food -= 1
+            print("I ate 1 food, now I have this much food left:",self.__food)
+        else:
+            print("No food left to eat")
+
+    def excrete_waste(self):
+        print("I have excreted my waste")
+
+    
 
