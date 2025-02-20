d33b05cd5ccb998b019afe1bd8fc4e425233a7c5
Tenphun <117119253+Tenphun@users.noreply.github.com>
2025-02-19 20:22:51 -0500

    2nd push

--- Animal.py
+++ Animal.py
@@ -1,32 +1,35 @@
 class Animal:
-    def __init__(self, name, species, gender, food=0):
+    def __init__(self, name, species, food=0):
         self.__name = name
         self.__species = species
-        self.__gender = gender
         self.__food = food
-        print("hello I am", self.__name)
+        print(f"hello I am {self.__name}.")
 
-
+    #Print's the animal's name
     def talk(self):
         print(self.__name)
 
+    #Prints the animal's species
     def print_species(self):
-        print("I am a",self.__species)
+        print(f"I am a {self.__species}.")
 
-    def print_gender(self):
-        print("I am a",self.__gender)
+    #Animal roaring
+    def roar(self):
+        print(f"{self.__name}: ROARRR!!")
 
+    #Gets food incraesing food count by 1
     def get_food(self):
         self.__food += 1
-        print("I have this much food:",self.__food)
+        print(f"I have this much food: {self.__food}.")
 
+    #Eats food decreasing food count by 1 and prints food count
     def eat_food(self):
         if self.__food > 0:
             self.__food -= 1
-            print("I ate 1 food, now I have this much food left:",self.__food)
+            print(f"I ate 1 food, now I have this much food left: {self.__food}.")
         else:
-            print("No food left to eat")
-
+            print(f"No food left to eat")
+    #Prints waste excreted
     def excrete_waste(self):
         print("I have excreted my waste")
 
